79515e21cdaa58a89bf2d5bf0c8bab1a8607157a
seppotk <seppo.kallio2@metropolia.fi>
2022-12-05 19:23:57 +0200

    Updates

---
+++ precache-manifest.0973ec4f55e21f5d35919e7714af63ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f76ce5d110689e97f817cb4d87e1e4a",
+    "url": "/seppotk/bonus_frontend.git/index.html"
+  },
+  {
+    "revision": "295ef9f11fbc83de3727",
+    "url": "/seppotk/bonus_frontend.git/static/css/main.bb1cd6a4.chunk.css"
+  },
+  {
+    "revision": "313ccda13af906be5d2b",
+    "url": "/seppotk/bonus_frontend.git/static/js/2.493e2401.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/seppotk/bonus_frontend.git/static/js/2.493e2401.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "295ef9f11fbc83de3727",
+    "url": "/seppotk/bonus_frontend.git/static/js/main.d93c5b0e.chunk.js"
+  },
+  {
+    "revision": "293d7c52939f9e115d0c",
+    "url": "/seppotk/bonus_frontend.git/static/js/runtime-main.bdc8013e.js"
+  }
+]);
